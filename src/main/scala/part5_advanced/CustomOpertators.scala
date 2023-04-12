d4c4aca8d0f3e21887d6bba1529323599a6431d7
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-04-12 12:26:48 +0530

    lecture 25

--- src/main/scala/part5_advanced/CustomOpertators.scala
+++ src/main/scala/part5_advanced/CustomOpertators.scala
@@ -6,6 +6,7 @@ import akka.stream.{
   Attributes,
   ClosedShape,
   CompletionStrategy,
+  FlowShape,
   Inlet,
   KillSwitches,
   Outlet,
@@ -26,10 +27,17 @@ import akka.stream.scaladsl.{
   Sink,
   Source
 }
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.stage.{
+  GraphStage,
+  GraphStageLogic,
+  GraphStageWithMaterializedValue,
+  InHandler,
+  OutHandler
+}
 import akka.util.Timeout
 
 import scala.collection.mutable
+import scala.concurrent.{Future, Promise}
 import scala.util.Random
 
 object CustomOpertators extends App {
@@ -115,5 +123,126 @@ object CustomOpertators extends App {
   }
 
   val batcherSink = Sink.fromGraph(new Batcher(10))
-  randomGeneratorSource.to(batcherSink).run()
+//  randomGeneratorSource.to(batcherSink).run()
+
+  /*
+  Exercise: a custom flow - a simple filter flow
+  - 2 ports: an input port and an output port
+
+   */
+
+  class CustomFlow[A](
+      predicate: A => Boolean
+  ) extends GraphStage[FlowShape[A, A]] {
+    val inputPort = Inlet[A]("filterIn")
+    val outputPort = Outlet[A]("filterOut")
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+
+        setHandler(
+          outputPort,
+          new OutHandler {
+            override def onPull(): Unit = pull(inputPort)
+          }
+        )
+
+        setHandler(
+          inputPort,
+          new InHandler {
+            override def onPush(): Unit = {
+              try {
+                val nextElem: A = grab[A](inputPort)
+                if (predicate(nextElem)) {
+                  push(outputPort, nextElem)
+                } else {
+                  pull(inputPort)
+                }
+              } catch {
+                case e: Throwable => failStage(e)
+              }
+            }
+          }
+        )
+      }
+
+    override def shape: FlowShape[A, A] = FlowShape[A, A](inputPort, outputPort)
+  }
+
+//  randomGeneratorSource.to(batcherSink).run()
+  val customFlow = Flow.fromGraph(
+    new CustomFlow[Int]((x: Int) => x % 2 == 0)
+  )
+//  randomGeneratorSource.via(customFlow).to(batcherSink).run()
+
+  /*
+  Materialized values in graph stages
+   */
+  // 3- a flow that counts the number of elements that go through it
+  class CounterFlow[A]
+      extends GraphStageWithMaterializedValue[FlowShape[A, A], Future[Int]] {
+    val inputPort = Inlet[A]("filterIn")
+    val outputPort = Outlet[A]("filterOut")
+    override val shape = FlowShape(inputPort, outputPort)
+
+    override def createLogicAndMaterializedValue(
+        inheritedAttributes: Attributes
+    ): (GraphStageLogic, Future[Int]) = {
+      val promise = Promise[Int]
+      val logic = new GraphStageLogic(shape) {
+        // setting mutable state
+        var counter = 0
+        setHandler(
+          outputPort,
+          new OutHandler {
+            override def onPull(): Unit = pull(inputPort)
+
+            override def onDownstreamFinish(cause: Throwable): Unit = {
+              promise.success(counter)
+              super.onDownstreamFinish(cause)
+            }
+          }
+        )
+        setHandler(
+          inputPort,
+          new InHandler {
+            override def onPush(): Unit = {
+              val nextElement = grab(inputPort)
+              counter += 1
+              // pass it on
+              push(outputPort, nextElement)
+            }
+
+            override def onUpstreamFinish(): Unit = {
+              promise.success(counter)
+              super.onUpstreamFinish()
+            }
+
+            override def onUpstreamFailure(ex: Throwable): Unit = {
+              promise.failure(ex)
+              super.onUpstreamFailure(ex)
+            }
+          }
+        )
+      }
+      (logic, promise.future)
+    }
+  }
+
+  import system.dispatcher
+  import scala.util.{Failure, Success}
+  val counterFlow = Flow.fromGraph(new CounterFlow[Int])
+  val countFuture = Source(1 to 10)
+    .viaMat(counterFlow)(Keep.right)
+    .to(
+      Sink.foreach[Int](x =>
+        if (x == 7) throw new RuntimeException("gotcha")
+        else println(x)
+      )
+    )
+    .run()
+  countFuture.onComplete {
+    case Success(value) => println(s"the number of elements passed: $value")
+    case Failure(ex)    => println(s"Counting the element failed: $ex")
+  }
 }
